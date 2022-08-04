bd2065290d93824e115ecb2b2645d89fa4afe87a
Alexis Peralta <agperalt@bancolombia.com.co>
2022-08-04 12:36:08 -0500

    Project start

---
+++ game/scripts/FishSO.gd
@@ -0,0 +1,12 @@
+extends Resource
+
+class_name FishSO
+
+export (String) var name
+export (String) var description
+export (String) var price
+export (String) var velocitySwin
+export (String) var velocityFight
+enum CATEGORY {SMALL, MEDIUM, BIG, LEGEND}
+export (CATEGORY) var categoryTipe
+export (Texture) var spriteSheet
